ddad6eed99b56f52d8603772a3885887268ab4a1
Lucie <lucie.genest@ecoco2.com>
2024-06-24 14:19:19 +0200

    [start_project] connection with database, create first model and update first get request with database data

--- back/app.js
+++ back/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const { password, userName, clusterName, dbName } = require("./conf");
 const app = express();
+
+const Cat = require("./models/cats");
+
+const dbURI = `mongodb+srv://${userName}:${password}@${clusterName}/${dbName}?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(dbURI)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -21,28 +31,13 @@ app.get("/", (req, res) => {
   res.send("GET request to the homepage");
 });
 
-app.get("/lost/cats", (req, res, next) => {
-  const cats = [
-    {
-      _id: "oeihfzeoi",
-      name: "Loupiau",
-      gender: "male",
-      imageUrl: "",
-      color: "brown",
-      description: "qsomihvqios",
-      department_number: 53,
-    },
-    {
-      _id: "oeihfzeoi",
-      name: "Loupiau",
-      gender: "male",
-      imageUrl: "",
-      color: "brown",
-      description: "qsomihvqios",
-      department_number: 53,
-    },
-  ];
-  res.status(200).json(cats);
+app.get("/lost/cats", async (req, res, next) => {
+  try {
+    const cats = await Cat.find();
+    res.status(200).json(cats);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 module.exports = app;
