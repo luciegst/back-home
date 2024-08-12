5f888fdce613ec18ff07bdbfddf4c9b75d646a68
luciegst <luciegenest@hotmail.fr>
2024-08-12 14:59:39 +0200

    [dogs_page] update message dogs page and update readme (#9)
    Co-authored-by: Lucie <lucie.genest@ecoco2.com>

--- front/src/views/LostDogsView.vue
+++ front/src/views/LostDogsView.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Banner from '@/components/ui/Banner.vue'
+</script>
 
-<template>Dogs Page</template>
+<template>
+  <Banner :title="'les chiens'" />
+  <!-- TODO in next US lost dogs part  -->
+  <div class="pt-14 pb-14 flex justify-center">Pas de chiens perdus.</div>
+</template>
 
 <style scoped></style>
